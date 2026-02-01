82e9c8fede894ac962c63f565c18ee8df0407671
Philip Rodrigues <philip.rodrigues.js@gmail.com>
2026-01-31 21:29:23 -0500

    commit

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -13,21 +13,3 @@ const ProtectedRoute = ({ children }) => {
 
 };
 
-// export default ProtectedRoute;
-
-// import { useAuth } from '../context/AuthContext';
-// import { Navigate } from 'react-router-dom';
-
-// const ProtectedRoute = ({ children }) => {
-//     const { session } = useAuth();
-
-//     if (session === undefined) {
-//         return <div>Loading...</div>;
-//     }
-
-//     return session ? <>{children}</> : <Navigate to="/signin" />;
-
-
-// };
-
-// export default ProtectedRoute;
